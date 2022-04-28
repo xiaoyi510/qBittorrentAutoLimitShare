2c6be5ed1471c7eeebc2c8dbdb0dcb1404ae814f
xiaoyi510 <loveyybin@qq.com>
2022-04-28 17:02:25 +0800

    移除

--- internal/service/service_db.go
+++ internal/service/service_db.go
@@ -1,28 +1,23 @@
 package service
 
-import (
-	"QbittorrentAutoLimitShare/internal/lib"
-	"log"
-)
+//var ServiceDb = &serviceDb{}
 
-var ServiceDb = &serviceDb{}
+//type serviceDb struct {
+//	Db *lib.DbSqlite
+//}
 
-type serviceDb struct {
-	Db *lib.DbSqlite
-}
-
-func (this *serviceDb) CheckUpdate() {
-	// 创建表
-	exec, err := this.Db.GetDb().Exec("CREATE TABLE if not exists \"tracker\" (\n  \"id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n  \"tracker\" TEXT,\n  \"limit_rate\" integer,\n  \"limit_time\" integer,\n  \"status\" integer\n)")
-	if err != nil {
-		log.Panicln(err)
-	}
-	log.Println(exec.RowsAffected())
-
-}
-
-func (this *serviceDb) Init() {
-	ServiceDb.Db = &lib.DbSqlite{}
-	ServiceDb.Db.Init().GetDb()
-	ServiceDb.CheckUpdate()
-}
+//func (this *serviceDb) CheckUpdate() {
+//	// 创建表
+//	exec, err := this.Db.GetDb().Exec("CREATE TABLE if not exists \"tracker\" (\n  \"id\" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n  \"tracker\" TEXT,\n  \"limit_rate\" integer,\n  \"limit_time\" integer,\n  \"status\" integer\n)")
+//	if err != nil {
+//		log.Panicln(err)
+//	}
+//	log.Println(exec.RowsAffected())
+//
+//}
+//
+//func (this *serviceDb) Init() {
+//ServiceDb.Db = &lib.DbSqlite{}
+//ServiceDb.Db.Init().GetDb()
+//ServiceDb.CheckUpdate()
+//}
--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"QbittorrentAutoLimitShare/internal/cmd"
 	"QbittorrentAutoLimitShare/internal/consts"
-	"QbittorrentAutoLimitShare/internal/service"
 )
 
 func main() {
-	service.ServiceDb.Init()
+	//service.ServiceDb.Init()
 	cmd.PublicWeb.Run()
 	//cmd.HandleCron.Run()
 	<-consts.Sig
