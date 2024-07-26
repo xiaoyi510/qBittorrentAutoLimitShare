b58bc61d9c25f7b848e3527238344af3a869310b
yuanbin.yang <yuanbin.yang@pitayagams.com>
2024-07-26 14:41:04 +0800

    优化代码

--- docker.sh
+++ docker.sh
@@ -4,4 +4,4 @@
 
 #goreleaser release
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./qbit-auto-limit .
-docker build -t xiaoyi510/qbit-auto-limit:v0.0.7 .
+docker build -t xiaoyi510/qbit-auto-limit:v0.0.7 -t xiaoyi510/qbit-auto-limit .
